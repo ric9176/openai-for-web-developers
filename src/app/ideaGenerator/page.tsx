30493b87d9fa431fd12ec01555b4fea0f034741a
Richard Moss <richard@englishtutorslive.com>
2023-10-16 16:35:56 +0100

    formatting

--- src/app/ideaGenerator/page.tsx
+++ src/app/ideaGenerator/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import {
   Box,
@@ -11,61 +11,56 @@ import {
   Spinner,
   Spacer,
   HStack,
-  VStack
+  VStack,
 } from '@chakra-ui/react';
 
 export default function SloganGenerator() {
-
-  const [idea, setIdea] = useState('')
+  const [idea, setIdea] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [generation, setGeneration] = useState(null)
-  const [imageSrc, setImageSrc] = useState(null)
+  const [generation, setGeneration] = useState(null);
+  const [imageSrc, setImageSrc] = useState(null);
 
   async function handleOnGenerateText(e: React.SyntheticEvent) {
     e.preventDefault();
-    setGeneration(null)
+    setGeneration(null);
 
     setIsLoading(true);
 
     const { data } = await fetch('/api/generateText', {
       method: 'POST',
       body: JSON.stringify({
-        prompt: idea
-      })
-    }).then(r => r.json());
+        prompt: idea,
+      }),
+    }).then((r) => r.json());
 
     setGeneration(data);
 
     const { image } = await fetch('/api/generateImage', {
       method: 'POST',
       body: JSON.stringify({
-        prompt: idea
-      })
-    }).then(r => r.json());
-    setImageSrc(image)
+        prompt: idea,
+      }),
+    }).then((r) => r.json());
+    setImageSrc(image);
 
     setIsLoading(false);
   }
 
   return (
-    <Container maxW="container.lg" pb={10}>
-      <VStack
-        spacing={6}
-        alignItems="center"
-        py={12}
-      >
-        <HStack spacing={2} w="60%" as="form" alignItems="center">
+    <Container maxW='container.lg' pb={10}>
+      <VStack spacing={6} alignItems='center' py={12}>
+        <HStack spacing={2} w='60%' as='form' alignItems='center'>
           <Input
             placeholder="What industry do you want to disrupt? e.g: 'travel'"
             value={idea}
             onChange={(e) => setIdea(e.target.value)}
-            maxW="lg"
-            width="100%"
+            maxW='lg'
+            width='100%'
           />
           <Spacer />
           <Button
             type='submit'
-            colorScheme="teal"
+            colorScheme='teal'
             onClick={handleOnGenerateText}
             disabled={!idea || isLoading}
             px={8}
@@ -77,39 +72,39 @@ export default function SloganGenerator() {
 
       {isLoading && (
         <Box
-          bg="whiteAlpha.600"
-          color="black"
-          borderRadius="lg"
+          bg='whiteAlpha.600'
+          color='black'
+          borderRadius='lg'
           m={{ sm: 4, md: 16, lg: 10 }}
           p={{ sm: 5, md: 5, lg: 16 }}
-          display="flex"
-          justifyContent="space-between"
+          display='flex'
+          justifyContent='space-between'
         >
           <Heading fontWeight={400}>Generating awesome idea...</Heading>
           <br />
           <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="black"
-            size="xl"
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='black'
+            size='xl'
           />
         </Box>
       )}
 
-      {!isLoading && generation && imageSrc &&
+      {!isLoading && generation && imageSrc && (
         <Box
-          bg="white"
-          color="black"
-          borderRadius="lg"
+          bg='white'
+          color='black'
+          borderRadius='lg'
           m={{ sm: 4, md: 16, lg: 10 }}
           p={{ base: 4, sm: 5, md: 5, lg: 16 }}
-          textAlign="center"
+          textAlign='center'
         >
           <Image alt='idea image' src={imageSrc} />
           <Text>{generation}</Text>
         </Box>
-      }
+      )}
     </Container>
   );
 }
--- src/app/ideaGeneratorStructured/page.tsx
+++ src/app/ideaGeneratorStructured/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { useState } from 'react';
 import {
   Box,
@@ -15,54 +15,54 @@ import {
 } from '@chakra-ui/react';
 
 export default function IdeaGenerator() {
-
-  const [idea, setIdea] = useState('')
+  const [idea, setIdea] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [generation, setGeneration] = useState(undefined)
-  const [imageSrc, setImageSrc] = useState(undefined)
+  const [generation, setGeneration] = useState(undefined);
+
+  const [imageSrc, setImageSrc] = useState(undefined);
 
   async function handleOnGenerateText(e: React.SyntheticEvent) {
     e.preventDefault();
 
-    const prompt = e.target.value
+    const prompt = e.target.value;
     setIsLoading(true);
     setIdea('');
-    setGeneration(undefined)
+    setGeneration(undefined);
 
     const { data } = await fetch('/api/generateData', {
       method: 'POST',
       body: JSON.stringify({
-        prompt
-      })
-    }).then(r => r.json());
+        prompt,
+      }),
+    }).then((r) => r.json());
 
     setGeneration(data);
 
     const { image } = await fetch('/api/generateImage', {
       method: 'POST',
       body: JSON.stringify({
-        prompt
-      })
-    }).then(r => r.json());
-    setImageSrc(image)
+        prompt,
+      }),
+    }).then((r) => r.json());
+    setImageSrc(image);
 
     setIsLoading(false);
   }
 
   return (
-    <Container maxW="container.lg" pb={10}>
-      <HStack spacing={2} w="60%">
+    <Container maxW='container.lg' pb={10}>
+      <HStack spacing={2} w='60%'>
         <Input
           placeholder="What industry do you want to disrupt? e.g: 'travel'"
           value={idea}
           onChange={(e) => setIdea(e.target.value)}
-          maxW="lg"
-          width="100%"
+          maxW='lg'
+          width='100%'
         />
         <Spacer />
         <Button
-          type="submit"
-          colorScheme="teal"
+          type='submit'
+          colorScheme='teal'
           onClick={handleOnGenerateText}
           disabled={!idea || isLoading}
           px={8}
@@ -72,44 +72,51 @@ export default function IdeaGenerator() {
 
         {isLoading && (
           <Box
-            bg="whiteAlpha.600"
-            color="black"
-            borderRadius="lg"
+            bg='whiteAlpha.600'
+            color='black'
+            borderRadius='lg'
             m={{ sm: 4, md: 16, lg: 10 }}
             p={{ sm: 5, md: 5, lg: 16 }}
-            display="flex"
-            justifyContent="space-between"
+            display='flex'
+            justifyContent='space-between'
           >
             <Heading fontWeight={400}>Generating awesome idea...</Heading>
             <br />
             <Spinner
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.200"
-              color="black"
-              size="xl"
+              thickness='4px'
+              speed='0.65s'
+              emptyColor='gray.200'
+              color='black'
+              size='xl'
             />
           </Box>
         )}
 
         {imageSrc && <Image alt='idea image' src={imageSrc} />}
-        {!isLoading && generation && < Box
-          bg="white"
-          color="black"
-          borderRadius="lg"
-          m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ base: 4, sm: 5, md: 5, lg: 16 }}
-          textAlign="center"
-        >
-          <Text><b>Product:</b> {generation && generation.idea}</Text>
-          <Text><b>Idea:</b> {generation && generation.idea}</Text>
-          <Text><b>Mission:</b> {generation && generation.mission}</Text>
-          {
-            generation && generation.uniqueSellingPoints &&
-            generation.uniqueSellingPoints.map((usp => <Text>{usp}</Text>))
-          }
-        </Box>}
+        {!isLoading && generation && (
+          <Box
+            bg='white'
+            color='black'
+            borderRadius='lg'
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ base: 4, sm: 5, md: 5, lg: 16 }}
+            textAlign='center'
+          >
+            <Text>
+              <b>Product:</b> {generation && generation.idea}
+            </Text>
+            <Text>
+              <b>Idea:</b> {generation && generation.idea}
+            </Text>
+            <Text>
+              <b>Mission:</b> {generation && generation.mission}
+            </Text>
+            {generation &&
+              generation.uniqueSellingPoints &&
+              generation.uniqueSellingPoints.map((usp) => <Text>{usp}</Text>)}
+          </Box>
+        )}
       </HStack>
-    </Container >
+    </Container>
   );
 }
