6894b99ef540486b8d738e2e84d7e595eace1b44
Richard Moss <richard@englishtutorslive.com>
2023-10-18 14:53:35 +0100

    Add streaming-http pkg and example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-
 import { useCompletion } from 'ai/react';
+import { useJsonStreaming } from 'http-streaming-request';
 import {
   Container,
   Text,
@@ -12,10 +12,11 @@ import {
 } from '@chakra-ui/react';
 
 export default function StartGenerator() {
-  const { completion, input, handleInputChange, handleSubmit, isLoading } =
-    useCompletion({
-      api: '/api/completionData',
-    });
+  const { data } = useJsonStreaming({
+    url: '/api/completionData',
+    method: 'POST',
+    payload: 'travel',
+  });
 
   return (
     <Container maxW='container.lg' pb={10}>
@@ -25,35 +26,35 @@ export default function StartGenerator() {
           w='60%'
           alignItems='center'
           as='form'
-          onSubmit={handleSubmit as any}
+          // onSubmit={handleSubmit as any}
         >
           <Input
             placeholder="What industry do you want to disrupt? e.g: 'travel'"
-            value={input}
-            onChange={handleInputChange}
+            // value={input}
+            // onChange={handleInputChange}
             maxW='lg'
             width='100%'
           />
           <Spacer />
-          <Button type='submit' colorScheme='teal' isLoading={isLoading} px={8}>
+          <Button type='submit' colorScheme='teal' px={8}>
             Generate Idea
           </Button>
         </HStack>
       </VStack>
 
-      {completion && (
-        <VStack
-          bg='whiteAlpha.800'
-          color='black'
-          borderRadius='lg'
-          m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ base: 4, sm: 5, md: 5, lg: 16 }}
-          alignItems='center'
-          spacing={6}
-        >
-          <Text>{completion}</Text>
-        </VStack>
-      )}
+      <VStack
+        bg='whiteAlpha.800'
+        color='black'
+        borderRadius='lg'
+        m={{ sm: 4, md: 16, lg: 10 }}
+        p={{ base: 4, sm: 5, md: 5, lg: 16 }}
+        alignItems='center'
+        spacing={6}
+      >
+        <Text>Product: {data && data.product}</Text>
+        <Text>Mission: {data && data.mission}</Text>
+        <Text>Idea: {data && data.idea}</Text>
+      </VStack>
     </Container>
   );
 }
